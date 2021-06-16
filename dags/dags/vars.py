56d510dd8db75bb0181ba88aedcf59d6f955ff41
GreasyGoose <androidegsensation@gmail.com>
2021-06-16 23:51:09 +0300

    Remove email

--- dags/dags/vars.py
+++ dags/dags/vars.py
@@ -4,7 +4,7 @@ VOLUME = 'D:/MADE/ML_prod/greasygoose/dags'
 
 default_args = {
         "owner": "greasygoose",
-        "email": ["androidegsensation@gmail.com"],
+        "email": ["myemail@gmail.com"],
         "retries": 1,
         "retry_delay": timedelta(minutes=5),
     }
